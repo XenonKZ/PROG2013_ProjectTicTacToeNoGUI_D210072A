f2def2a35bb2ad17cf3e67df2508d995383dd6e3
XenonKZ <37468434+XenonKZ@users.noreply.github.com>
2024-01-05 18:17:01 +0800

    Add files via upload
    Board and Java is required for Tic Tac Toe to function.
    TicTacToe.Java is the main function.

---
+++ Board.java
@@ -0,0 +1,75 @@
+package Project;
+
+public class Board {
+	public static final int SIZE = 3;	// the size of the square, standard tic tac toe is 3 x 3 can make it into other sizes if needed
+    private char[][] grid;
+
+    public Board() {
+        grid = new char[SIZE][SIZE];	//make a board matrix
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                grid[i][j] = ' ';
+            }
+        }
+    }
+
+    public void displayBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(grid[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+    
+    public static int getSize() {
+        return SIZE;
+    }
+
+    public boolean isCellEmpty(int row, int col) {
+        return grid[row][col] == ' ';
+    }
+
+    public void markCell(int row, int col, char symbol) {
+        grid[row][col] = symbol;
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (grid[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkWin(char symbol) {
+        // Checking rows and columns
+        for (int i = 0; i < SIZE; i++) {
+            if (grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol) {
+                return true; // If row have same symbol = win
+            }
+            if (grid[0][i] == symbol && grid[1][i] == symbol && grid[2][i] == symbol) {
+                return true; // If column have same symbol = win
+            }
+        }
+
+        // Check diagonals
+        if (grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol) {
+            return true; // Diagonal win (top left to bottom right)
+        }
+        if (grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol) {
+            return true; // Diagonal win (top right to bottom left)
+        }
+
+        return false;
+    }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,86 @@
+package Project;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Board board;
+    private Player currentPlayer;
+
+    public TicTacToe() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter name for Player 1: ");	//get player1 name
+        String player1Name = scanner.nextLine();
+
+        System.out.println("Enter name for Player 2: ");	//get player2 name
+        String player2Name = scanner.nextLine();
+        
+        player1 = new Player(player1Name, 'X');
+        player2 = new Player(player2Name, 'O');
+        board = new Board();
+        currentPlayer = player1;
+    }
+
+    public void play() {
+    	Scanner scanner = new Scanner(System.in);
+        
+        System.out.println();
+        
+        do {
+            board.displayBoard();
+            int row, col;
+
+            do {
+                System.out.println(currentPlayer.getName() + ", enter your move (row and column) [|0|1|2|] : ");
+                row = scanner.nextInt();
+                col = scanner.nextInt();
+            } while (!isValidMove(row, col));
+
+            board.markCell(row, col, currentPlayer.getSymbol());
+
+            if (board.checkWin(currentPlayer.getSymbol())) {
+                board.displayBoard();
+                System.out.println(currentPlayer.getName() + " wins!");
+                break;
+            } else if (board.isBoardFull()) {
+                board.displayBoard();
+                System.out.println("It's a draw!");
+                break;
+            }
+
+            switchPlayer();
+            System.out.println();		//add a empty line
+        } while (true);
+
+        scanner.close();
+    }
+
+    private boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= Board.SIZE || col < 0 || col >= Board.SIZE) {
+            System.out.println("Invalid move. Row and column must be between 0 and " + (Board.SIZE - 1) + ".");
+            return false;
+        }
+
+        if (!board.isCellEmpty(row, col)) {
+            System.out.println("Invalid move. The cell is already occupied.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void switchPlayer() {  
+        if (currentPlayer == player1) {		// Check if it's currently player1's turn
+            currentPlayer = player2;		// If yes, switch to player2 for the next turn
+        } else {
+            currentPlayer = player1;		// If it's not player1's turn, switch to player1 for the next turn
+        }
+    }//or can use this alternate line |currentPlayer = (currentPlayer == player1) ? player2 : player1;|
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+}
